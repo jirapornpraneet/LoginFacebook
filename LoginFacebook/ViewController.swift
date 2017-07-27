cfeacd6b0a1d472794aad2e1575044ffa71c9091
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-07-27 17:44:37 +0700

    Login เชื่อมกับ facebook โดยดึงชื่อและรูปภาพที่รับค่ามาจาก facebook มาโชว์

--- LoginFacebook/ViewController.swift
+++ LoginFacebook/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 import FacebookLogin
 import FBSDKLoginKit
 import FBSDKCoreKit
+import SDWebImage
 
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+      @IBOutlet weak var lbShow: UILabel!
+      @IBOutlet weak var imageShow: UIImageView!
     
     let loginButton: FBSDKLoginButton = {
         let button = FBSDKLoginButton()
@@ -24,24 +27,30 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         view.addSubview(loginButton)
         loginButton.center = view.center
         loginButton.delegate = self
-        if let token = FBSDKAccessToken.current(){
+        if let token = FBSDKAccessToken.current() {
             fetchProfile()
         }
     }
     func fetchProfile(){
-        print("fetchProfile")
         let parameters = ["fields" : "email, first_name, last_name, picture.type(large)"]
         FBSDKGraphRequest(graphPath: "me", parameters: parameters).start { (connection, result, error) in
+            if let username = (result as AnyObject)["first_name"]! as? String {
+                self.lbShow.text = "Hi " + username
+            }else {
+                self.lbShow.text = ""
+            }
+            
             if error != nil {
                 print(error)
             }
             if let email = (result as AnyObject)["email"]! as? String{
                 print(email)
             }
+            
             if let picture = (result as AnyObject)["picture"]! as? NSDictionary,let data = picture["data"] as? NSDictionary,
                 let url = data["url"]as? String {
                 print(url)
-                
+                self.imageShow.sd_setImage(with: URL(string: url))
             }
         }
     }
@@ -51,6 +60,8 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         fetchProfile()
     }
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        self.lbShow.text = ""
+        self.imageShow.image = nil
     }
     func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
         return true
