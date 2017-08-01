5f7db5df2e75609d091a8312ece32987f7187ec6
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-08-01 17:35:39 +0700

    ให้ข้อมูลเพื่อนในเฟสบุ้คโชว์ใน CollectionView

--- LoginFacebook/CollectionViewController.swift
+++ LoginFacebook/CollectionViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import FacebookLogin
+import FBSDKLoginKit
+import FBSDKCoreKit
+import FBSDKShareKit
+import SDWebImage
 
 private let reuseIdentifier = "Cell"
 
+class FriendsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageViewAvatar: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
+
 class CollectionViewController: UICollectionViewController {
     let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+    var getToken:FBSDKAccessToken!
+    
+    var friendsResource: FriendsResource! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
-        layout.itemSize = CGSize(width: 90, height: 90)
+        layout.itemSize = CGSize(width: 110, height: 110)
         self.collectionView?.collectionViewLayout = layout
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.collectionView?.delegate = self
+        self.collectionView?.dataSource = self
+        
+        if  let token = FBSDKAccessToken.current() {
+            getToken = token
+            print(getToken.tokenString)
+            print("Show >>> ",token.tokenString)
+        }
+        getDataCurrenciesAPI(showToken: getToken)
     }
+    
+    
+    var getJson = JSON([String: Any]())
+    func getDataCurrenciesAPI(showToken: FBSDKAccessToken) {
+        let url = String(format:"https://graph.facebook.com/me/taggable_friends?fields=name,picture&access_token=%@",showToken.tokenString)
+        Alamofire.request(url, method: .get).validate().responseString { response in
+            print(response)
+            switch response.result {
+            case .success(let value):
+                self.friendsResource  = FriendsResource(json: value)
+                print(value)
+                print(self.friendsResource)
+                self.collectionView?.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -30,38 +75,24 @@ class CollectionViewController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 11
+        if ((friendsResource) != nil) {
+            return friendsResource.data!.count
+        } else {
+            return 0
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath)
-        configureCell(cell, forItemAtIndexPath: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! FriendsCollectionViewCell
         
-        cell.layer.borderWidth=1.0;
-        cell.layer.borderColor=UIColor.blue.cgColor;
+        let cellData = friendsResource.data?[indexPath.row]
+        
+        cell.nameLabel.text = String(format: "%@", (cellData?.name)!)
+        cell.imageViewAvatar.sd_setImage(with: URL(string: (cellData?.picture?.data?.url)!), completed: nil)
         
         return cell
     }
     
-    func configureCell(_ cell: UICollectionViewCell, forItemAtIndexPath: IndexPath) {
-        cell.backgroundColor = UIColor.black
-        //3
-        let imgView = UIImageView(frame: CGRect(x: 0, y: 0, width: layout.itemSize.width, height: layout.itemSize.height))
-        imgView.contentMode = .scaleAspectFit
-        if forItemAtIndexPath.row%3 == 0 {
-            imgView.image = UIImage(named: "img3.jpg")
-        } else if forItemAtIndexPath.row%2 == 0 {
-            imgView.image = UIImage(named: "img2.jpg")
-        } else {
-            imgView.image = UIImage(named: "img1.jpg")
-        }
-        cell.addSubview(imgView)
-        
-    }
-    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let view =  collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "collectionCell", for: indexPath) as UICollectionReusableView
-        return view
-    }
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
     }
