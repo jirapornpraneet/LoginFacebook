9889f0a20bf8b68d215bc3e6cac1e5bd73f0e49e
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-08-01 13:06:15 +0700

    เอาค่า JSON. รายชื่อเพื่อนเฟสบุ้คออกมา

--- LoginFacebook/ViewController.swift
+++ LoginFacebook/ViewController.swift
@@ -10,27 +10,53 @@ import UIKit
 import FacebookLogin
 import FBSDKLoginKit
 import FBSDKCoreKit
+import FBSDKShareKit
 import SDWebImage
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
       @IBOutlet weak var lbShow: UILabel!
       @IBOutlet weak var imageShow: UIImageView!
+      @IBOutlet weak var showFriendButton : UIButton!
     
     let loginButton: FBSDKLoginButton = {
         let button = FBSDKLoginButton()
-        button.readPermissions = ["email"]
+        button.readPermissions = ["email", "user_friends", "user_about_me"]
         return button
     }()
-
+    var getToken:FBSDKAccessToken!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(loginButton)
         loginButton.center = view.center
         loginButton.delegate = self
-        if let token = FBSDKAccessToken.current() {
+    
+        if  let token = FBSDKAccessToken.current() {
             fetchProfile()
+            getToken = token
+            print(getToken.tokenString)
+            getDataCurrenciesAPI(showToken: token)
+            print("Show >>> ",token.tokenString)
         }
     }
+    
+    var getJson = JSON([String: Any]())
+    func getDataCurrenciesAPI(showToken: FBSDKAccessToken) {
+        let url = String(format:"https://graph.facebook.com/me/taggable_friends?fields=name,picture&access_token=%@",showToken.tokenString)
+        Alamofire.request(url, method: .get).validate().responseJSON { response in
+            print(response)
+            switch response.result {
+            case .success(let value):
+                self.getJson = JSON(value)
+                print("ShowJson : %@", self.getJson)
+                print("ShowJson : ",self.getJson)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
     func fetchProfile(){
         let parameters = ["fields" : "email, first_name, last_name, picture.type(large)"]
         FBSDKGraphRequest(graphPath: "me", parameters: parameters).start { (connection, result, error) in
@@ -54,6 +80,56 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
             }
         }
     }
+    
+    @IBAction func listFriendTouchUpInside(_ sender: Any) {
+        getDataCurrenciesAPI(showToken: getToken!)
+        print(getToken.tokenString)
+        self.performSegue(withIdentifier: "ShowKidney", sender: sender)
+        
+    }
+//    func friendsList() {
+//        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me/friends", parameters: nil)
+//        graphRequest.start( completionHandler: { (connection, result, error) -> Void in
+//            
+//            if ((error) != nil)
+//            {
+//                // Process error
+//                print("Error: \(error)")
+//                return
+//            }
+//            
+//            let summary = (result as AnyObject)["summary"] as! NSDictionary
+//            let counts = (summary as AnyObject)["total_count"] as! NSNumber
+//            
+//            let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me/taggable_friends", parameters: ["fields": "id,name,picture,url", "limit": "1"])
+//            graphRequest.start( completionHandler: { (connection, result, error) -> Void in
+//                
+//                if ((error) != nil)
+//                {
+//                    print("Error: \(error)")
+//                    return
+//                }
+//                else
+//                {
+//                    print(result)
+////                    let friends = (result as AnyObject)["data"] as! NSArray
+////                    var count = 1
+////                    if let array = friends as? [NSDictionary] {
+////                        for friend : NSDictionary in array {
+////                            let name = (friend as AnyObject)["name"] as! NSString
+////                            let picture = (friend as AnyObject)["picture"]! as! NSDictionary
+////                            let data = picture["data"]! as! NSDictionary
+////                            let url = data["url"]! as! String
+////                            print("\(count) \(name) \(picture) \(url)")
+////                            count += 1
+////                        }
+////                    }
+//                }
+//                
+//            })
+//        })
+//    }
+    
     
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
         print("completed login")
