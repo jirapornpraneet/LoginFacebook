e80aa9f7e52c52d5d0396f54a6045ea654bf5c71
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-08-03 11:06:03 +0700

    สร้าง Class เก็บข้อมูลโปรไฟล์ของผู้ใช้

--- LoginFacebook/UserResource.swift
+++ LoginFacebook/UserResource.swift
@@ -16,6 +16,88 @@ class UserResource: EVObject {
     var first_name: String = ""
     var last_name: String = ""
     var picture: PictureUserData?
+    var about:String = ""
+    var age_range:AgeRangeData?
+    var birthday:String = ""
+    var gender:String = ""
+    var cover:CoverUserData?
+    var hometown:HomeTownData?
+    var work:[WorkData]? = []
+    var education:[EducationData]? = []
+    var post:PostData?
+}
+class PostData:EVObject{
+    var data:[PostUserData]? = []
+}
+
+class PostUserData:EVObject{
+    var message:String = ""
+    var picture:String = ""
+    var id:Int = 0
+}
+
+class WorkData: EVObject {
+    var end_date:String = ""
+    var employer:EmployerData?
+    var location:LocationData?
+    var position:PositionData?
+    var start_date:String = ""
+    var id:Int = 0
+}
+
+class PositionData: EVObject {
+    var id:Int = 0
+    var name:String = ""
+}
+
+
+
+class LocationData:EVObject{
+    var id:Int = 0
+    var name:String = ""
+}
+
+class EmployerData: EVObject {
+    var id:Int = 0
+    var name:String = ""
+}
+
+class EducationData: EVObject {
+    var school:SchoolData?
+    var type:String = ""
+    var year:YearData?
+    var id:Int = 0
+    var concentration:[ConcentrationData]? = []
+}
+
+class ConcentrationData: EVObject {
+    var id:Int = 0
+    var name:String = ""
+}
+
+class YearData:EVObject{
+    var id:Int = 0
+    var name:String = ""
+}
+
+class SchoolData: EVObject {
+    var id:Int = 0
+    var name:String = ""
+}
+
+class HomeTownData:EVObject{
+    var id: Int = 0
+    var name:String = ""
+}
+class CoverUserData:EVObject{
+    var id:Int = 0
+    var offset_x:String = ""
+    var offset_y:String = ""
+    var source:String = ""
+}
+
+class AgeRangeData: EVObject {
+    var min:String = ""
 }
 
 class PictureUserData: EVObject {
