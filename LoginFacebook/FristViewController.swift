79929e012043c06b47baeed31ba50687345edf37
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-08-04 14:53:43 +0700

    เพิ่มปุ่ม LoginFacebook  ในหน้าแรกที่เข้าแอพ

--- LoginFacebook/FristViewController.swift
+++ LoginFacebook/FristViewController.swift
@@ -1,35 +1,87 @@
 //
-//  FristViewController.swift
+//  ViewController.swift
 //  LoginFacebook
 //
-//  Created by Jiraporn Praneet on 8/4/2560 BE.
+//  Created by Jiraporn Praneet on 7/27/2560 BE.
 //  Copyright © 2560 Jiraporn Praneet. All rights reserved.
 //
 
 import UIKit
+import FacebookLogin
+import FBSDKLoginKit
+import FBSDKCoreKit
+import FBSDKShareKit
+import SDWebImage
+import Alamofire
+import SwiftyJSON
+import SKPhotoBrowser
 
-class FristViewController: UIViewController {
 
+class FristViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+
+    
+    var userResource: UserResource! = nil
+    var loginButton: FBSDKLoginButton = {
+        let button = FBSDKLoginButton()
+        button.readPermissions = ["email", "user_friends", "user_about_me"]
+        return button
+    }()
+    
+    var getToken:FBSDKAccessToken!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.addSubview(loginButton)
+        profileImageView.layer.masksToBounds = true
+        profileImageView.layer.cornerRadius = 4
+        profileImageView.layer.borderWidth = 2
+        profileImageView.layer.borderColor = UIColor.white.cgColor
+        loginButton.center = view.center
+        loginButton.delegate = self
+     
+        if  let token = FBSDKAccessToken.current() {
+            fetchProfile()
+            //            getToken = token
+            //            print(getToken.tokenString)
+            print("Show >>> ",token.tokenString)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func fetchProfile(){
+        let parameters = ["fields" : "email, first_name, last_name, picture.type(large), about, age_range, birthday, gender, cover, hometown, work,education,posts{created_time,message,full_picture,place}"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start { (connection, result, error) in
+            let dic = result as? NSDictionary
+            let jsonString = dic?.toJsonString()
+            //            print("Result :",result)
+            //            print("Dic : ",dic)
+            //            print("json :",jsonString)
+            self.userResource = UserResource(json: jsonString)
+            //            print("UserResource :", self.userResource)
+            self.nameLabel.text = self.userResource.first_name + "  " + self.userResource.last_name
+            self.profileImageView.sd_setImage(with: URL(string: (self.userResource.picture?.data?.url)!), completed: nil)
+                   }
     }
-    */
-
+    
+    @IBAction func listFriendTouchUpInside(_ sender: Any) {
+        self.performSegue(withIdentifier: "CollectionViewID", sender: sender)
+        
+    }
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("completed login")
+        fetchProfile()
+    }
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        self.nameLabel.text = ""
+        self.profileImageView.image = nil
+    }
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
+
