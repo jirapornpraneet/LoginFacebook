2e5ba13b3e0f653839c5deec4dd6c925ae3dfc92
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-08-11 13:59:20 +0700

    ให้ข้อมูลใน tableViewCell ออกมาตามจำนวนที่มีอยู่ใน post ของเพื่อน

--- LoginFacebook/DetailFriendViewController.swift
+++ LoginFacebook/DetailFriendViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 import SDWebImage
 import SKPhotoBrowser
+import FacebookLogin
+import FBSDKLoginKit
+import FBSDKCoreKit
+import FBSDKShareKit
+import Alamofire
+import SwiftyJSON
 
-class DetailFriendViewController: UIViewController {
+
+class PostFriendTableViewCell: UITableViewCell {
+    @IBOutlet weak var picturePostImageView: UIImageView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var profilePostImageView: UIImageView!
+    @IBOutlet weak var namePostLabel: UILabel!
+    @IBOutlet weak var createdTimePostLabel: UILabel!
+    @IBOutlet weak var placePostLabel: UILabel!
+    @IBOutlet weak var atPlaceLabel: UILabel!
+    @IBOutlet weak var iconCheckInImageView: UIImageView!
+}
+
+class DetailFriendViewController: UIViewController ,UITableViewDelegate, UITableViewDataSource{
+    @IBOutlet var tablePostFriend: UITableView!
     @IBOutlet weak var imgImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var birthdayLabel: UILabel!
     @IBOutlet weak var genderLabel: UILabel!
     @IBOutlet weak var educationLabel: UILabel!
     @IBOutlet weak var hometownLabel: UILabel!
+    @IBOutlet weak var messengerToFriendLabel: UILabel!
     @IBOutlet weak var coverImage: UIImageView!
     @IBOutlet weak var educationImage: UIImageView!
     @IBOutlet weak var hometownImage: UIImageView!
+    @IBOutlet weak var profileUserImage: UIImageView!
     
     var getName = String()
     var getPictureDataURL = String()
@@ -31,6 +52,15 @@ class DetailFriendViewController: UIViewController {
     var getEducationImage = UIImage()
     var getHometownImage = UIImage()
     
+    var userResource: UserResource! = nil
+    
+    var getCreatedTime = String()
+    var getMeaasge = String()
+    var getFullPicture = String()
+    var getPlace = String()
+    var getCountPostFriends = Int()
+    var friendsResource = FriendsResource()
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         imgImage.layer.masksToBounds = true
@@ -46,12 +76,30 @@ class DetailFriendViewController: UIViewController {
         educationLabel.text = getEducation
         educationImage.image = getEducationImage
         hometownImage.image = getHometownImage
+        messengerToFriendLabel.text = String(format: "%เขียนอะไรบางอย่างถึง  %@ %.........",getName)
         coverImage.sd_setImage(with: URL(string: (getCoverImage)), completed: nil)
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(DetailFriendViewController.ZoomPictureDataURL))
         imgImage.addGestureRecognizer(tap)
         imgImage.isUserInteractionEnabled = true
+        
+        tablePostFriend.dataSource = self
+        tablePostFriend.delegate = self
+        self.tablePostFriend.reloadData()
+        
+        getProfileUser()
     }
+    
+    func getProfileUser(){
+        let parameters = ["fields" : "email, first_name, last_name, picture.type(large), about, age_range, birthday, gender, cover, hometown, work,education,posts{created_time,message,full_picture,place}"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start { (connection, result, error) in
+            let dic = result as? NSDictionary
+            let jsonString = dic?.toJsonString()
+            self.userResource = UserResource(json: jsonString)
+            self.profileUserImage.sd_setImage(with: URL(string: (self.userResource.picture?.data?.url)!), completed: nil)
+        }
+    }
+
     
     func ZoomPictureDataURL(){
         // 1. create URL Array
@@ -70,4 +118,37 @@ class DetailFriendViewController: UIViewController {
         super.didReceiveMemoryWarning()
 
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if ((getCountPostFriends) != nil) {
+            return getCountPostFriends
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PostFriendTableViewCell
+//        let cellData = friendsResource.data?[indexPath.row]
+//        let cellPostData = cellData?.posts?.data?[indexPath.row]
+//        print("Post :",cellPostData)
+  
+        cell.messageLabel.text = "Message"
+        cell.picturePostImageView = nil
+        cell.namePostLabel.text = getName
+        cell.createdTimePostLabel.text = getName
+        cell.placePostLabel.text = ""
+        cell.profilePostImageView.image = nil
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
 }
+
