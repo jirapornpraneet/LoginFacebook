e212a38b85d5df05bb2bacd39814091f9e105084
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-08-15 14:39:01 +0700

    สร้างคลาส FunctionHelper โดยมีฟังก์ชั่น ดึงURL โดยใช้ pod  OCThumbor

---
+++ Function Helper/FunctionHelper.swift
@@ -0,0 +1,21 @@
+//
+//  FunctionHelper.swift
+//  LoginFacebook
+//
+//  Created by Jiraporn Praneet on 8/15/2560 BE.
+//  Copyright © 2560 Jiraporn Praneet. All rights reserved.
+//
+
+import Foundation
+import OCThumbor
+
+class FunctionHelper: NSObject {
+    
+    func getThumborUrlFromImageUrl(imageUrlStr: String, width: Int32, height: Int32) -> URL {
+        let thumbor : OCThumbor = OCThumbor.create(withHost: Constants.kTHUMBOR_URL, key: Constants.kTHUMBOR_SECURE_KEY)
+        let builder : OCThumborURLBuilder = thumbor.buildImage(imageUrlStr)
+        builder.resizeSize = ResizeSizeMake(width, height)
+        return URL(string: builder.toUrlSafe())!
+    }
+}
+
