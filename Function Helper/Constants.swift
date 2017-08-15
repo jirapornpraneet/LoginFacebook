ba864427106bdddbdd20a28ed70eb3e3775e44df
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-08-15 14:36:04 +0700

    สร้างคลาส Constants เก็บค่าคงที่

---
+++ Function Helper/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  LoginFacebook
+//
+//  Created by Jiraporn Praneet on 8/15/2560 BE.
+//  Copyright © 2560 Jiraporn Praneet. All rights reserved.
+//
+
+import Foundation
+
+class Constants: NSObject {
+    static let kTHUMBOR_SECURE_KEY = "thumbor_secure_key"
+    static let kTHUMBOR_URL = "https://img.vorchat.com"
+}
+
