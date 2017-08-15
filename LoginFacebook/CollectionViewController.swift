78ffb36e5359e15076c3d80bcb9b2df0563ec6ce
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-08-15 10:41:38 +0700

    ให้ข้อมูล posts ที่มีอักขระพิเศษ {} ออกมาโดยการทำให้ url.stringByAddingPercentEncodingWithAllowedCharacters

--- LoginFacebook/CollectionViewController.swift
+++ LoginFacebook/CollectionViewController.swift
@@ -47,22 +47,25 @@ class CollectionViewController: UICollectionViewController {
     
     var getJson = JSON([String: Any]())
     func getDataCurrenciesAPI() {
-        let url = String(format:"https://graph.facebook.com/me/friends?fields=name,picture.type(large),birthday,gender,cover,education,hometown,posts&access_token=EAACEdEose0cBACjB14PKUxGiJhcwq8BniIoN8FAzkExZBKMClFhI9zvA3IBDD8s92fBbfY5e4nmZCtEezkmVd1l49BQdHX8oaiwwyZBc6JZAlEeXZAcRagVJZBPFy606GcO41K1JBFiURJF5omJZAGxuQTOj69ZCK7UOQEm4n3svkpanWXjUb14UScE34gGZBr4i83iQkFpU4mAZDZD")
+        var url = String(format:"https://graph.facebook.com/me/friends?fields=name,picture.type(large),birthday,gender,cover,education,hometown,posts{message,full_picture,created_time,place}&access_token=EAACEdEose0cBAC1CSwP1qjc8HYW8krIMYTcBtjEEnlIVDcx2nn4Em9xnywPSVoOnDLjK5YdHX9p3xAfrr3YB4AbHS1qk1ptZBPnWa4OwZAAujdDU5GdMJsa96SZAICbx81GwPE9DtGSUxSbyRrqPYZBZBqmfZBJtVUPcYHnmLM5ZAiHri1aBGHZBbDPBTHOvudcriBvmyo06XQZDZD")
+        url = url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        print("URL",url)
+//        addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+//        url = url.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
         Alamofire.request(url, method: .get).validate().responseString { response in
             print(response)
             switch response.result {
             case .success(let value):
                 self.friendsResource  = FriendsResource(json: value)
                 print("FriendsResource :",self.friendsResource)
-                print(value)
+//                print(value)
                 self.collectionView?.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
     }
-
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -91,6 +94,7 @@ class CollectionViewController: UICollectionViewController {
         let cellCollectionView = MainStoryboard.instantiateViewController(withIdentifier: "DetailFriendViewController") as! DetailFriendViewController
         
         let cellData = friendsResource.data?[indexPath.row]
+        
         
         cellCollectionView.getPictureDataURL = (cellData?.picture?.data?.url)!
         cellCollectionView.getName = String(format: "%@", (cellData?.name)!)
@@ -128,23 +132,28 @@ class CollectionViewController: UICollectionViewController {
             cellCollectionView.getHometownImage = UIImage(named: "iconHometown.png")!
         }
        
+//        let cellResource = friendsResource.data
+//        cellCollectionView.getFriendsResource = (cellResource)!
+        
         let cellCount = cellData?.posts?.data?.count
         cellCollectionView.getCountPostFriends = (cellCount)!
+       
+        let cellDataPost = cellData?.posts?.data
+        print("cellDataPost :",cellDataPost?[0])
+        cellCollectionView.getPostsIndexPath = cellDataPost!
+                
+//        cellCollectionView.getCreatedTime = (cellDataPost?.created_time)!
+//        cellCollectionView.getMeaasge = (cellDataPost?.message)!
+//        let myLocale = Locale(identifier: "th_TH")
+//        let dateStringFormResource = cellDataPost?.created_time
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+//        let date = dateFormatter.date(from: dateStringFormResource!)
+//        dateFormatter.locale = myLocale
+//        dateFormatter.dateFormat = "EEEE" + " เวลา " + "hh:mm"
+//        let dateString = dateFormatter.string(from: date!)
         
-        let cellDataPost = cellData?.posts?.data?[indexPath.row]
-        cellCollectionView.getCreatedTime = (cellDataPost?.created_time)!
-        cellCollectionView.getMeaasge = (cellDataPost?.message)!
-        
-        let myLocale = Locale(identifier: "th_TH")
-        let dateStringFormResource = cellDataPost?.created_time
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        let date = dateFormatter.date(from: dateStringFormResource!)
-        dateFormatter.locale = myLocale
-        dateFormatter.dateFormat = "EEEE" + " เวลา " + "hh:mm"
-        let dateString = dateFormatter.string(from: date!)
-        
-        cellCollectionView.getCreatedTime = dateString
+//        cellCollectionView.getCreatedTime = dateString
 //        cellCollectionView.getFullPicture = (cellDataPost?.full_picture)!
 //        cellCollectionView.getPlace = (cellDataPost?.place?.name)!
         
