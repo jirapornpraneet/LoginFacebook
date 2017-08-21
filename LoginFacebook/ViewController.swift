f32ce4d2798d3bd23a2a04e0c11ef676955f5df7
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-08-21 10:36:41 +0700

    เพิ่มฟิลด์  photos.limit(10) ให้โชว์รูปภาพ 10 รูปในอัลบั้ม

--- LoginFacebook/ViewController.swift
+++ LoginFacebook/ViewController.swift
@@ -98,12 +98,12 @@ class ViewController: UITableViewController {
     }
    
     func fetchProfile() {
-        let parameters = ["fields": "email, first_name, last_name, picture.type(large), about, age_range, birthday, gender, cover, hometown, work, education, posts{created_time, message, full_picture, place}, albums{created_time, count, description,name, photos.limit(1){picture,name}}"]
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large), about, age_range, birthday, gender, cover, hometown, work, education, posts{created_time, message, full_picture, place}, albums{created_time, count, description,name, photos.limit(10){picture,name}}"]
         FBSDKGraphRequest(graphPath: "me", parameters: parameters).start { (_, result, _) in
             let dic = result as? NSDictionary
             let jsonString = dic?.toJsonString()
             self.userResourceData = UserResourceData(json: jsonString)
-            
+            print("UserResource", self.userResourceData)
             self.nameLabel.text = self.userResourceData.first_name + "  " + self.userResourceData.last_name
             self.schoolNameLabel.text = self.userResourceData.education?[2].school?.name
             self.concentrationNameLabel.text = self.userResourceData.education?[2].concentration?[0].name
