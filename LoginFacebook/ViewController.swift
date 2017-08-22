297a23f023caf7bbbe401cb409bdf4c11d2a3fc0
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-08-22 11:42:18 +0700

    เพิ่มฟิล์ดโชว์ข้อมูล likes{name,link,pic_large}

--- LoginFacebook/ViewController.swift
+++ LoginFacebook/ViewController.swift
@@ -96,7 +96,7 @@ class ViewController: UITableViewController {
     }
    
     func fetchProfile() {
-        let parameters = ["fields": "email, first_name, last_name, picture.type(large), about, age_range, birthday, gender, cover, hometown, work, education, posts{created_time, message, full_picture, place}, albums{created_time, count, description,name, photos.limit(10){picture,name}}"]
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large), about, age_range, birthday, gender, cover, hometown, work, education, posts{created_time, message, full_picture, place,likes{name,pic_large,link}}, albums{created_time, count, description,name, photos.limit(10){picture,name}}"]
         FBSDKGraphRequest(graphPath: "me", parameters: parameters).start { (_, result, _) in
             let resultDictionary = result as? NSDictionary
             let jsonString = resultDictionary?.toJsonString()
