ef80cfed335f4b1a8848376eb48f84fd71805236
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-09-06 10:54:09 +0700

    ฟังก์ชั่นดึงข้อมูลผู้ใช้ออกมาโชว์ใน Label

--- ShowProfileUser/AboutProfileViewController.swift
+++ ShowProfileUser/AboutProfileViewController.swift
@@ -32,24 +32,51 @@ class AboutProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var userResourceData: UserResourceData! = nil
+    
+    func fetchUserResourceProfile() {
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large), about, age_range, birthday, gender, cover, hometown, work, education,location,relationship_status, music.limit(4){name,picture{url},category},movies.limit(4){global_brand_page_name,picture{url}}"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start { (_, result, _) in
+            let resultDictionary = result as? NSDictionary
+            let jsonString = resultDictionary?.toJsonString()
+            self.userResourceData = UserResourceData(json: jsonString)
+            
+            let userResourceDataWork = self.userResourceData.work?[0]
+            let userResourceDataWorkPositionName = userResourceDataWork?.position?.name
+            let userResourceDataWorkEmployerName = userResourceDataWork?.employer?.name
+            self.employerNameLabel.text = String(format:"%@ %ที่ %@", userResourceDataWorkPositionName!, userResourceDataWorkEmployerName!)
+            
+            let dateStringFormUserResourceDataWorkStartDate = userResourceDataWork?.start_date
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            let date = dateFormatter.date(from: dateStringFormUserResourceDataWorkStartDate!)
+            dateFormatter.dateFormat = "yyyy"
+            let dateString = dateFormatter.string(from: date!)
+            self.yearWorkedLabel.text = String(format:"%@ %ถึงปัจจุบัน", dateString)
+            
+            let userResourceDataLocal = self.userResourceData.location?.name
+            self.locationLabel.text = String(format:"%อาศัยอยู่ที่ %@", userResourceDataLocal!)
+            
+            self.fromLocationLabel.text = String(format:"%จาก %@", userResourceDataLocal!)
+            
+            let userResourceDataEducationConcentrationName  = self.userResourceData.education?[2].concentration?[0].name
+            let userResourceDataEducationSchoolName = self.userResourceData.education?[2].school?.name
+            self.schoolNameLabel.text = String(format:"%เรียน %@ %ที่ %@",userResourceDataEducationConcentrationName!, userResourceDataEducationSchoolName!)
+            
+            let userResourceDataEducationSchoolName0 = self.userResourceData.education?[0].school?.name
+            let userResourceDataEducationSchoolName1 = self.userResourceData.education?[1].school?.name
+            self.previousStudyLabel.text = String(format:"%ก่อนหน้า: %@ %และ %@", userResourceDataEducationSchoolName0!, userResourceDataEducationSchoolName1!)
+            
+            let userResourceDataRelationShip = self.userResourceData.relationship_status
+            self.ralationshipLabel.text = userResourceDataRelationShip
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
 
 }
