e2ad55591835ff9ab537ea26f55241844dd54514
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-09-21 14:16:37 +0700

    - เพิ่มฟิลด์ใน parameter - ดึงข้อมูลรูปภาพมาในตัวแปร thumborProfileUpdateImageUrl - กำหนดรูปภาพ ให้ UI profileImageButton

--- ViewController/AllChoiceViewController.swift
+++ ViewController/AllChoiceViewController.swift
@@ -20,7 +20,7 @@ class AllChoiceViewController: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var profileImageButton: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchUserResourceProfile()
@@ -30,28 +30,26 @@ class AllChoiceViewController: UIViewController {
         profileImageButton.layer.borderWidth = 2
         profileImageButton.layer.borderColor = UIColor.white.cgColor
     }
-
+    
     var userResourceData: UserResourceData! = nil
     
     func fetchUserResourceProfile() {
-        let parameters = ["fields": "first_name, last_name, picture{url}, groups{name,picture{url}}"]
+        let parameters = ["fields": "first_name, last_name, picture.type(large),games"]
         FBSDKGraphRequest(graphPath: "me", parameters: parameters).start { (_, result, _) in
             let resultDictionary = result as? NSDictionary
             let jsonString = resultDictionary?.toJsonString()
             self.userResourceData = UserResourceData(json: jsonString)
-            print("userResourceData", self.userResourceData)
-            
             let userResourceDataName = self.userResourceData.first_name + "  " + self.userResourceData.last_name
             self.nameLabel.text = userResourceDataName
             
-            let thumborProfileImageUrl = FunctionHelper().getThumborUrlFromImageUrl(imageUrlStr: (self.userResourceData.picture?.data?.url)!, width: 200, height: 200)
-            self.profileImageButton.sd_setBackgroundImage(with: thumborProfileImageUrl, for: .normal, completed: nil)
+            let thumborProfileUpdateImageUrl = FunctionHelper().getThumborUrlFromImageUrl(imageUrlStr: (self.userResourceData.picture?.data?.url)!, width: 200, height: 200)
+            self.profileImageButton.sd_setBackgroundImage(with: thumborProfileUpdateImageUrl, for: .normal, completed: nil)
+            
         }
     }
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
 }
